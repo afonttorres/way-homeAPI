f30d4a35602cce6490770a45af30ca493412a8e0
Agnès Font Torres <afonttorres@gmail.com>
2022-07-22 05:22:52 +0200

    MatchServiceTest

--- src/test/java/com/hackaton/wayhome/services/match/MatchServiceTest.java
+++ src/test/java/com/hackaton/wayhome/services/match/MatchServiceTest.java
@@ -1,10 +1,27 @@
 package com.hackaton.wayhome.services.match;
 
+import com.hackaton.wayhome.dtos.MatchReqDto;
+import com.hackaton.wayhome.exceptions.NotFoundException;
+import com.hackaton.wayhome.models.Housing;
+import com.hackaton.wayhome.models.Match;
+import com.hackaton.wayhome.models.Seller;
+import com.hackaton.wayhome.models.User;
+import com.hackaton.wayhome.repositories.IHousingRepository;
 import com.hackaton.wayhome.repositories.IMatchRepository;
+import com.hackaton.wayhome.repositories.IUserRepository;
+import com.hackaton.wayhome.services.housing.HousingService;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -12,16 +29,141 @@ class MatchServiceTest {
 
     @Mock
     IMatchRepository matchRepository;
+    @Mock
+    IUserRepository userRepository;
+    @Mock
+    IHousingRepository housingRepository;
 
     @Test
-    void getAll() {
+    void getAllShouldReturnAllMatches() {
+        int n = 4;
+        var service = new MatchService(matchRepository, userRepository, housingRepository);
+        var matches = this.createMatches(n);
+        Mockito.when(matchRepository.findAll()).thenReturn(matches);
+        var sut = service.getAll();
+        assertThat(sut.size(), equalTo(n));
     }
 
     @Test
-    void getUserMatches() {
+    void getUserMatchesShouldReturnUserMatchesByPamId() {
+        int n = 5;
+        var service = new MatchService(matchRepository, userRepository, housingRepository);
+        var user = this.createUser(1L);
+        var matches = this.createMatches(n);
+        Mockito.when(userRepository.findById(any(Long.class))).thenReturn(Optional.of(user));
+        Mockito.when(matchRepository.findByUserId(any(Long.class))).thenReturn(matches);
+        var sut = service.getUserMatches(1L);
+        assertThat(sut, equalTo(matches));
     }
 
     @Test
-    void create() {
+    void getUserMatchesShouldntReturnMatchesIfUserDoesntExistAndThrowsException(){
+        var service = new MatchService(matchRepository, userRepository, housingRepository);
+        Exception ex = assertThrows(NotFoundException.class, ()->{
+            service.getUserMatches(1L);
+        });
+        String msg = "User Not Found";
+        var sut = ex.getMessage();
+        assertTrue(sut.equals(msg));
+    }
+
+    @Test
+    void createShouldPostAMatchAndReturnAMatchResDto() {
+        var service = new MatchService(matchRepository, userRepository, housingRepository);
+        var match = this.createMatch(1L);
+        MatchReqDto req = new MatchReqDto(1L ,1L);
+        Mockito.when(housingRepository.findById(any(Long.class))).thenReturn(Optional.of(match.getHousing()));
+        Mockito.when(userRepository.findById(any(Long.class))).thenReturn(Optional.of(match.getUser()));
+        Mockito.when(housingRepository.save(any(Housing.class))).thenReturn(match.getHousing());
+        Mockito.when(matchRepository.save(any(Match.class))).thenReturn(match);
+        var sut = service.create(req);
+        assertThat(sut.getSeller(), equalTo(match.getSeller()));
+    }
+
+    @Test
+    void createShouldntCreateMatchIfHousingDoesntExist() {
+        var service = new MatchService(matchRepository, userRepository, housingRepository);
+        var req = new MatchReqDto(1L, 1L);
+        Exception ex = assertThrows(NotFoundException.class, ()->{
+            service.create(req);
+        });
+        String msg = "Housing Not Found";
+        var sut = ex.getMessage();
+        assertTrue(sut.equals(msg));
+    }
+
+    @Test
+    void createShouldntCreateMatchIfUserDoesntExist() {
+        var service = new MatchService(matchRepository, userRepository, housingRepository);
+        var req = new MatchReqDto(1L, 1L);
+        var housing = this.createHousing(1L);
+        Mockito.when(housingRepository.findById(any(Long.class))).thenReturn(Optional.of(housing));
+        Exception ex = assertThrows(NotFoundException.class, ()->{
+            service.create(req);
+        });
+        String msg = "User Not Found";
+        var sut = ex.getMessage();
+        assertTrue(sut.equals(msg));
+    }
+
+    private Housing createHousing(Long id){
+        var housing = new Housing();
+        var seller = this.createSeller(id);
+        housing.setTitle("title");
+        housing.setDescription("desc");
+        housing.setCategory("cat");
+        housing.setActive(true);
+        housing.setImgUrl1("img");
+        housing.setImgUrl2("img");
+        housing.setImgUrl3("img");
+        housing.setLocation("loc");
+        housing.setRatio(4);
+        housing.setPrice(400D);
+        housing.setPublisher(seller);
+        housing.setId(id);
+        return housing;
+    }
+
+    private Seller createSeller(Long id){
+        var seller = new Seller();
+        seller.setName("name");
+        seller.setUsername("username");
+        seller.setEmail("email");
+        seller.setAvatarUrl("avatar");
+        seller.setLocation("loc");
+        seller.setId(id);
+        return seller;
+    }
+
+    private User createUser(Long id){
+        var user = new User();
+        user.setName("name");
+        user.setUsername("user");
+        user.setEmail("email");
+        user.setAnimal("animal");
+        user.setLocation("loc");
+        user.setAvatarUrl("avatar");
+        user.setId(id);
+        return user;
+    }
+
+    private Match createMatch(Long id){
+        var seller = this.createSeller(id);
+        var user = this.createUser(id);
+        var housing = this.createHousing(id);
+        var match = new Match();
+        match.setSeller(seller);
+        match.setUser(user);
+        match.setHousing(housing);
+        match.setId(id);
+        return match;
+    }
+
+    private List<Match> createMatches(int n){
+        List<Match> matches = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            matches.add(this.createMatch(Long.valueOf(i)));
+        }
+        return matches;
     }
 }
