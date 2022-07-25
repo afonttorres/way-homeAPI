bb15d02a9981ea2271bf24aa5891da0388fdbdb0
Agnès Font Torres <afonttorres@gmail.com>
2022-07-25 10:38:47 +0200

    pre checkout commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,31 +1,37 @@
 INSERT
 INTO
-  users
-  (name, surname, username, email, phone, password, avatar, country, citty, created_Date)
+users
+(name, surname, username, email, phone, password, avatar, country, citty, created_Date)
 VALUES
-  ('agnes', 'font', 'afont', 'aft@gmail.com', 666666666, 'password', 'avatar', 'catalunya', 'la garriga', NOW());
+('agnes', 'font', 'afont', 'aft@gmail.com', 666666666, 'password', 'avatar', 'catalunya', 'la garriga', NOW());
 
 INSERT
 INTO
-  publishers
-  (name, surname, username, email, phone, password, avatar, country, citty, created_Date)
+publishers
+(name, surname, username, email, phone, password, avatar, country, citty, created_Date)
 VALUES
-  ('lola', 'martinez', 'lolamartinez', 'lolaf5@gmail.com', 777777777, 'password', 'avatar', 'catalunya', 'barcelona', NOW());
+('lola', 'martinez', 'lolamartinez', 'lolaf5@gmail.com', 777777777, 'password', 'avatar', 'catalunya', 'barcelona', NOW());
 INSERT
 INTO
-  users
-  (name, surname, username, email, password, created_Date)
+users
+(name, surname, username, email, password, created_Date)
 VALUES
-  ('nil', 'font', 'nfont', 'nft@gmail.com', 'password', NOW());
+('nil', 'font', 'nfont', 'nft@gmail.com', 'password', NOW());
 INSERT
 INTO
-  pets
-  (name, type, user_id)
+pets
+(name, type, user_id)
 VALUES
-  ('Ron', 0, 1);
+('Ron', 0, 1);
 INSERT
 INTO
-  pets
-  (name, type, user_id)
+pets
+(name, type, user_id)
 VALUES
-  ('Sira', 0, 1);
+('Sira', 0, 1);
+INSERT
+INTO
+pets
+(name, type, user_id)
+VALUES
+('Ron', 0, 2);
